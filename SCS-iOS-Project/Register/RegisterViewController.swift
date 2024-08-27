58b5af4de2bd6d3b1256bb86ba28eb770b431c41
Johan Gatica <johan.g.arevalo@gmail.com>
2024-08-27 16:53:19 -0500

    listado cancha completo, modelos para reserva completos.

---
+++ SCS-iOS-Project/Register/RegisterViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterViewController.swift
+//  SCS-iOS-Project
+//
+//  Created by Diego Gatica on 26/08/24.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nombresTextField: UITextField!
+    @IBOutlet weak var apellidosTextField: UITextField!
+    @IBOutlet weak var dniTextField: UITextField!
+    @IBOutlet weak var correoTextField: UITextField!
+    @IBOutlet weak var contrasenaTextField: UITextField!
+    @IBOutlet weak var repitContrasenaTextField: UITextField!
+    @IBOutlet weak var telefonoTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SCS-iOS-Project/Reserva/ReservaViewController.swift
+++ SCS-iOS-Project/Reserva/ReservaViewController.swift
@@ -8,22 +8,42 @@
 import UIKit
 
 class ReservaViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var reserva: [ReservaResponse] = []
+    var sedes: [Int: Sede] = [:]
+    var cancha: [Int: CanchaResponse] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-    */
+    
+    func navigateToEditSedeViewController(reserva: ReservaResponse) {
+        let storyboard = UIStoryboard(name: "Sede", bundle: nil)
+        if let formRegisterViewController = storyboard.instantiateViewController(withIdentifier: "FormularioRegisterViewController") as? FormularioReservaViewController {
+            formRegisterViewController.reserva = reserva
+            self.navigationController?.pushViewController(formRegisterViewController, animated: true)
+        }
+    }
+    
+}
 
+extension ReservaViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reserva.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
